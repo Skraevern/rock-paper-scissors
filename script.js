8f43a909b9c03ec0be2cf564729ac6a96818ac78
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-04-09 17:52:22 +0200

    Added click event listeners to buttons and functions for selection

--- script.js
+++ script.js
@@ -16,69 +16,58 @@ let computerPlay = function () {
   }
 };
 
-// Ensures correct capitalization of prompt
-let capitalize = function (inputString) {
-  let firstLetter = inputString.substring(1, -9999);
-  let firstLetterCapitalize = firstLetter.toUpperCase();
-  let remainingString = inputString.slice(1);
-  let lowerCaseRemaining = remainingString.toLowerCase();
-  return `${firstLetterCapitalize}${lowerCaseRemaining}`;
-};
-
 // Game
-let singleGame = function (player, computer) {
+let game = function (player, computer) {
   if (player === computer) {
     playerScore++;
     computerScore++;
-    return `Draw! Both picked ${player}.`;
+    return console.log(`Draw! Both picked ${player}.`);
   }
   if (player === `Rock`) {
     if (computer === `Scissors`) {
       playerScore++;
-      return `Player wins. Rock beats Scissors!`;
+      return console.log(`Player wins. Rock beats Scissors!`);
     } else {
       computerScore++;
-      return `Computer wins! Paper beats Scissors!`;
+      return console.log(`Computer wins! Paper beats Rock!`);
     }
   }
   if (player === `Scissors`) {
-    if (computer === `Rock`) {
+    if (computer === `Paper`) {
       computerScore++;
-      return `Computer wins! Rock beats Scissors!`;
+      return console.log(`Player wins! Scissors beat Paper!`);
     } else {
       playerScore++;
-      return `Player wins! Scissors beat Paper!`;
+      return console.log(`Computer wins! Rock beats Scissors!`);
     }
   }
   if (player === `Paper`) {
     if (computer === `Rock`) {
       playerScore++;
-      return `Player wins! Paper beats Rock!`;
+      return console.log(`Player wins! Paper beats Rock!`);
     } else {
       computerScore++;
-      return `Computer wins! Scissors beat Paper!`;
+      return console.log(`Computer wins! Scissors beat Paper!`);
     }
   }
 };
 
-// Plays the game five times, displays each rounds winner and the total winner after 5 games.
-let fiveGames = function () {
-  for (let index = 0; index < 5; index++) {
-    let playerPrompt = prompt();
-    let playerSelection = capitalize(playerPrompt);
-    let computerSelection = computerPlay();
-
-    console.log(
-      `Player picked ${playerSelection}. 
-       Computer picked ${computerSelection}`
-    );
-    console.log(singleGame(playerSelection, computerSelection));
-  }
-  if (playerScore > computerScore) {
-    console.log(`Player wins! ${playerScore} - ${computerScore}`);
-  } else {
-    console.log(`Computer wins 5 games! ${computerScore} - ${playerScore}`);
-  }
+let playRound = function (playerSelection) {
+  game(playerSelection, computerPlay());
 };
 
-fiveGames();
+const playRoundRock = function () {
+  return playRound(`Rock`);
+};
+const playRoundPaper = function () {
+  return playRound(`Paper`);
+};
+const playRoundScissors = function () {
+  return playRound(`Scissors`);
+};
+
+document.getElementById(`btn-rock`).addEventListener(`click`, playRoundRock);
+document.getElementById(`btn-paper`).addEventListener(`click`, playRoundPaper);
+document
+  .getElementById(`btn-scissors`)
+  .addEventListener(`click`, playRoundScissors);
